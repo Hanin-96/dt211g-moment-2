91415d67c1fb6174ffda2024b40945a0c5850067
Hanin Farhan <haninfarhan96@gmail.com>
2024-01-26 14:00:03 +0100

    Sorterat kod, lagt till still på tabellen i css

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,27 @@ async function loadSchedule() {
     try {
         const response = await fetch("https://dahlgren.miun.se/ramschema_ht23.php");
         const data = await response.json();
-        console.log(data);
+        console.table(data);
 
         //Sorterar kurser på kurskod
-        const schedules = data.sort((itemA, itemB) => (itemA.code > itemB.code) ? 1 : -1);
+        const schedules = data.sort((itemA, itemB) => (itemA.code < itemB.code) ? 1 : -1);
         schedules.forEach(schedule => {
             document.getElementById("getSchedule").innerHTML += `<tr><td>${schedule.code}</td><td>${schedule.coursename}</td><td>${schedule.progression}</td></tr>`;
         });
 
-        
     } catch {
         document.getElementById("getSchedule").innerHTML = "Det går inte läsa av listan"
     }
 }
 
 loadSchedule();
+
+function sortCourse() {
+    //Sorterar kurser på kurskod
+    const schedules = data.sort((itemA, itemB) => (itemA.code > itemB.code) ? 1 : -1);
+    schedules.forEach(schedule => {
+        document.getElementById("getSchedule").innerHTML += `<tr><td>${schedule.code}</td><td>${schedule.coursename}</td><td>${schedule.progression}</td></tr>`;
+    });
+}
+
+sortCourse();
