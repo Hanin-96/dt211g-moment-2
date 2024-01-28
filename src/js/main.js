044f21b5db74e778fbf3cd7766e8767d4969633c
Hanin Farhan <haninfarhan96@gmail.com>
2024-01-28 22:03:52 +0100

    id på tabellrader +  eventlistener på tabell kolumner + sortera tabell

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ async function loadSchedule() {
     try {
         const response = await fetch("https://dahlgren.miun.se/ramschema_ht23.php");
         data = await response.json();
-        console.table(data);
+        console.log(data);
 
         //Sorterar kurser på kurskod
         const schedules = data.sort((itemA, itemB) => (itemA.code < itemB.code) ? 1 : -1);
@@ -21,26 +21,33 @@ async function loadSchedule() {
 
 loadSchedule();
 
-//Skapar eventlistener för klick på kurskod
-document.getElementById("course-code").addEventListener("click", sortCourse);
-
 let sortedDown = true;
-function sortCourse() {
+//Skapar eventlistener för klick på kurskod
+document.getElementById("course-code").addEventListener("click", () => {
+    sort("code");
+});
+
+document.getElementById("course-name").addEventListener("click", () => {
+    sort("coursename");
+});
+
+document.getElementById("course-progression").addEventListener("click", () => {
+    sort("progression");
+});
+
+function sort(sortBy) {
     document.getElementById("getSchedule").innerHTML = "";
 
     let schedules = [];
     if (sortedDown === true) {
-        schedules = data.sort((itemA, itemB) => (itemA.code > itemB.code) ? 1 : -1);
+        schedules = data.sort((itemA, itemB) => (itemA[sortBy] > itemB[sortBy]) ? 1 : -1);
         sortedDown = false;
     } else {
-        schedules = data.sort((itemA, itemB) => (itemA.code < itemB.code) ? 1 : -1);
+        schedules = data.sort((itemA, itemB) => (itemA[sortBy] < itemB[sortBy]) ? 1 : -1);
         sortedDown = true;
     }
     schedules.forEach(schedule => {
         document.getElementById("getSchedule").innerHTML += `<tr><td>${schedule.code}</td><td>${schedule.coursename}</td><td>${schedule.progression}</td></tr>`;
     });
 }
-
-
-
 
