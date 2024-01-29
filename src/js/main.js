bf7d190db3e666d499e1f40e214b8b3411f4c69d
Hanin Farhan <haninfarhan96@gmail.com>
2024-01-29 14:12:38 +0100

    Filtrera input text i sökfält + fixat css för sökfält

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,28 @@
 "use strict"
 
+// Data variablen sparar all data som hämtats via fetch-anrop  
 let data = [];
 
+// Funktion för att hämta och skriva ut data i tabell
 async function loadSchedule() {
     try {
         const response = await fetch("https://dahlgren.miun.se/ramschema_ht23.php");
         data = await response.json();
-        console.log(data);
 
         //Sorterar kurser på kurskod
-        const schedules = data.sort((itemA, itemB) => (itemA.code < itemB.code) ? 1 : -1);
+        const schedules = data;
         schedules.forEach(schedule => {
             document.getElementById("getSchedule").innerHTML += `<tr><td>${schedule.code}</td><td>${schedule.coursename}</td><td>${schedule.progression}</td></tr>`;
         });
 
     } catch {
+         //Ifall ingen utskrift av data kan ske i tabellen ska följande skrivas ut
         document.getElementById("getSchedule").innerHTML = "Det går inte läsa av listan"
     }
 }
 
 loadSchedule();
 
-let sortedDown = true;
 //Skapar eventlistener för klick på kurskod
 document.getElementById("course-code").addEventListener("click", () => {
     sort("code");
@@ -36,18 +37,40 @@ document.getElementById("course-progression").addEventListener("click", () => {
 });
 
 function sort(sortBy) {
+    //rensar innehåll i tabell för att skriva ut ny ordning
     document.getElementById("getSchedule").innerHTML = "";
 
     let schedules = [];
-    if (sortedDown === true) {
+    
         schedules = data.sort((itemA, itemB) => (itemA[sortBy] > itemB[sortBy]) ? 1 : -1);
-        sortedDown = false;
-    } else {
-        schedules = data.sort((itemA, itemB) => (itemA[sortBy] < itemB[sortBy]) ? 1 : -1);
-        sortedDown = true;
-    }
+
+
     schedules.forEach(schedule => {
         document.getElementById("getSchedule").innerHTML += `<tr><td>${schedule.code}</td><td>${schedule.coursename}</td><td>${schedule.progression}</td></tr>`;
     });
 }
+
+//Filtrera innehåll beroende på input i sökfält
+
+//Eventlistener på sökfältet
+document.getElementById("search").addEventListener("keyup", () => {
+  searchInput();
+});
+
+function searchInput() {
+  //Deklarera variabler
+  let searchText = document.getElementById("search");
+  let input = searchText.value.toUpperCase();
+
+  document.getElementById("getSchedule").innerHTML = "";
+
+  let schedules = data.filter((schedule) => schedule.code.toUpperCase().includes(input) || schedule.coursename.toUpperCase().includes(input));
+  console.log(schedules);
+
+  schedules.forEach(schedule => {
+    document.getElementById("getSchedule").innerHTML += `<tr><td>${schedule.code}</td><td>${schedule.coursename}</td><td>${schedule.progression}</td></tr>`;
+});
+
+}
+
 
