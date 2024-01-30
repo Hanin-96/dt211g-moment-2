0e0707bb9089403c21e8b4eafc7e5967e906b6dd
Hanin Farhan <haninfarhan96@gmail.com>
2024-01-30 10:36:20 +0100

    Fixade struktur och rensade kod

--- src/js/main.js
+++ src/js/main.js
@@ -3,59 +3,68 @@
 // Data variablen sparar all data som hämtats via fetch-anrop  
 let data = [];
 
-// Funktion för att hämta och skriva ut data i tabell
-async function loadSchedule() {
-    try {
-        const response = await fetch("https://dahlgren.miun.se/ramschema_ht23.php");
-        data = await response.json();
-
-        //Sorterar kurser på kurskod
-        const schedules = data;
-        schedules.forEach(schedule => {
-            document.getElementById("getSchedule").innerHTML += `<tr><td>${schedule.code}</td><td>${schedule.coursename}</td><td>${schedule.progression}</td></tr>`;
-        });
-
-    } catch {
-         //Ifall ingen utskrift av data kan ske i tabellen ska följande skrivas ut
-        document.getElementById("getSchedule").innerHTML = "Det går inte läsa av listan"
-    }
-}
-
-loadSchedule();
-
-//Skapar eventlistener för klick på kurskod
-document.getElementById("course-code").addEventListener("click", () => {
-    sort("code");
-});
-
-document.getElementById("course-name").addEventListener("click", () => {
-    sort("coursename");
-});
-
-document.getElementById("course-progression").addEventListener("click", () => {
-    sort("progression");
-});
-
-function sort(sortBy) {
-    //rensar innehåll i tabell för att skriva ut ny ordning
-    document.getElementById("getSchedule").innerHTML = "";
-
-    let schedules = [];
-    
-        schedules = data.sort((itemA, itemB) => (itemA[sortBy] > itemB[sortBy]) ? 1 : -1);
-
-
-    schedules.forEach(schedule => {
-        document.getElementById("getSchedule").innerHTML += `<tr><td>${schedule.code}</td><td>${schedule.coursename}</td><td>${schedule.progression}</td></tr>`;
-    });
-}
-
-//Filtrera innehåll beroende på input i sökfält
-
 //Eventlistener på sökfältet
 document.getElementById("search").addEventListener("keyup", () => {
   searchInput();
 });
+
+//Skapar eventlistener för klick på kurskod
+document.getElementById("course-code").addEventListener("click", () => {
+  sort("code");
+});
+
+//Skapar eventlistener för klick på kursnamn
+document.getElementById("course-name").addEventListener("click", () => {
+  sort("coursename");
+});
+
+//Skapar eventlistener för klick på progression
+document.getElementById("course-progression").addEventListener("click", () => {
+  sort("progression");
+});
+
+//Funktioner:
+
+loadSchedule();
+
+// Funktion för att hämta och skriva ut data i tabell
+async function loadSchedule() {
+  try {
+    const response = await fetch("https://dahlgren.miun.se/ramschema_ht23.php");
+    data = await response.json();
+    displaySchedule(data);
+
+  } catch {
+    //Ifall ingen utskrift av data kan ske i tabellen ska följande skrivas ut
+    document.getElementById("getSchedule").innerHTML = "Det går inte läsa av listan"
+  }
+}
+
+//Sorterar kurser på kurskod
+function displaySchedule(schedules) {
+//Itererar igenom och skriver ut data i tabellform
+  schedules.forEach(schedule => {
+    document.getElementById("getSchedule").innerHTML += 
+    `<tr>
+    <td>${schedule.code}</td>
+    <td>${schedule.coursename}</td>
+    <td>${schedule.progression}</td>
+    </tr>`;
+  });
+}
+
+function sort(sortBy) {
+  //rensar innehåll i tabell för att skriva ut ny ordning
+  document.getElementById("getSchedule").innerHTML = "";
+
+  let schedules = [];
+
+  schedules = data.sort((itemA, itemB) => (itemA[sortBy] > itemB[sortBy]) ? 1 : -1);
+
+  displaySchedule(schedules);
+}
+
+//Filtrera innehåll beroende på input i sökfält
 
 function searchInput() {
   //Deklarera variabler
@@ -64,12 +73,10 @@ function searchInput() {
 
   document.getElementById("getSchedule").innerHTML = "";
 
-  let schedules = data.filter((schedule) => schedule.code.toUpperCase().includes(input) || schedule.coursename.toUpperCase().includes(input));
-  console.log(schedules);
+  let schedules = data.filter((schedule) => 
+  schedule.code.toUpperCase().includes(input) || schedule.coursename.toUpperCase().includes(input));
 
-  schedules.forEach(schedule => {
-    document.getElementById("getSchedule").innerHTML += `<tr><td>${schedule.code}</td><td>${schedule.coursename}</td><td>${schedule.progression}</td></tr>`;
-});
+ displaySchedule(schedules);
 
 }
 
